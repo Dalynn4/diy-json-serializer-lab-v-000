d4bc924ac4b26d5c2100a2b80506532999695955
Randall Robinson <keith_adams007@yahoo.com>
2018-06-14 17:40:13 +0000

    automatically backed up by learn

--- spec/features/product_feature_spec.rb
+++ spec/features/product_feature_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe "Products", type: :feature do
   end
 
   it 'requires javascript to go next' do
-   ge).not_to have_content p2.name
+  
   end
 
   it 'loads next product without page refresh', js: true do
